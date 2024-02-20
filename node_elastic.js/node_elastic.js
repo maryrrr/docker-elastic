74ce87c0f750bc48d703a1916fef8c9c58f66983
maryrrr <mary.russu83@gmail.com>
2024-02-20 08:56:16 +0100

    add more

--- node_elastic.js/node_elastic.js
+++ node_elastic.js/node_elastic.js
@@ -34,3 +34,57 @@ async function searchElastic(searchTerm, startDate = null, endDate = null) {
 module.exports = {
     searchElastic
 }
+// Search.jsx (Manejo de la búsqueda):
+// jsx
+// Copy code
+// import React, { useEffect, useContext, useState } from 'react';
+// import { GlobalContext } from '../../context/GlobalState';
+// import SearchResults from './SearchResults'; // Importa el componente de resultados
+
+// const Search = () => {
+//     const { searchDocs } = useContext(GlobalContext);
+//     const [searchTerm, setSearchTerm] = useState('');
+//     const [startDate, setStartDate] = useState('');
+//     const [endDate, setEndDate] = useState('');
+//     const [searchResults, setSearchResults] = useState([]);
+
+//     const handleSubmit = (event) => {
+//         event.stopPropagation();
+//         event.preventDefault();
+//         console.log("Lo que quiero buscar es: ", searchTerm);
+//         let response = searchDocs(searchTerm, startDate, endDate);
+//         setSearchResults(response); // Actualiza los resultados de búsqueda
+//     }
+
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit}>
+//                 {/* Formulario de búsqueda */}
+//             </form>
+
+//             {/* Componente para mostrar los resultados */}
+//             <SearchResults results={searchResults} />
+//         </div>
+//     )
+// }
+
+// export default Search;
+// SearchResults.jsx (Visualización de resultados):
+// jsx
+// Copy code
+// import React from 'react';
+
+// const SearchResults = ({ results }) => {
+//     return (
+//         <div>
+//             {/* Muestra los resultados aquí */}
+//             {results.map((result, index) => (
+//                 <div key={index}>
+//                     {/* Renderiza cada resultado */}
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
+
+// export default SearchResults;
